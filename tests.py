3b02b0f833108c35ed13c2fde2ed5b1b6348720c
Yulia Belisova <belisova@mail.ru>
2025-10-26 16:44:01 +0700

    All tests done

--- tests.py
+++ tests.py
@@ -1,24 +1,76 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    import pytest
+
+    @pytest.mark.parametrize(
+        "name,genre",
+        [("Шерлок", "Детективы"), ("Оно", "Ужасы"), ("Звездные войны", "Фантастика")],
+    )
+    def test_set_book_genre_add_existed_name_and_genre(self, name, genre):
         collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert collector.books_genre[name] == genre
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_book_genre_for_existing_book(self):
+        collector = BooksCollector()
+        collector.add_new_book("Оно")
+        collector.set_book_genre("Оно", "Ужасы")
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert collector.get_book_genre("Оно") == "Ужасы"
+
+    def test_get_book_genre_for_book_without_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Книга без жанра")
+
+        assert collector.get_book_genre("Книга без жанра") == ""
+
+    def test_get_books_with_specific_genre_horror(self):
+        collector = BooksCollector()
+        collector.add_new_book("Оно")
+        collector.set_book_genre("Оно", "Ужасы")
+
+        assert collector.get_books_with_specific_genre("Ужасы") == ["Оно"]
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Оно")
+        collector.set_book_genre("Оно", "Ужасы")
+
+        assert collector.get_books_genre() == {"Оно": "Ужасы"}
+
+    def test_get_books_for_children_with_books_for_more_than_18yo(self):
+        collector = BooksCollector()
+        collector.add_new_book("Оно")
+        collector.add_new_book("Шерлок")
+        collector.set_book_genre("Оно", "Ужасы")
+        collector.set_book_genre("Шерлок", "Детективы")
+
+        assert collector.get_books_for_children() == []
+
+    def test_add_book_in_favorites_when_name_is_not_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Оно")
+        collector.add_book_in_favorites("Оно")
+
+        assert collector.favorites == ["Оно"]
+
+    def test_delete_book_from_favorites_multiple_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Звездные войны")
+        collector.add_new_book("Оно")
+        collector.add_book_in_favorites("Оно")
+        collector.add_book_in_favorites("Звездные войны")
+        collector.delete_book_from_favorites("Звездные войны")
+
+        assert collector.favorites == ["Оно"]
+
+    def test_get_list_of_favorites_books_list_is_empty(self):
+        collector = BooksCollector()
+        collector.add_new_book("Звездные войны")
+
+        assert collector.get_list_of_favorites_books() == []
